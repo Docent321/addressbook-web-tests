07b5de9a21ffc9c8372cae49037f4a9d178d3834
Alex_Donskov <mrad@list.ru>
2023-04-17 20:14:18 +0300

    Добавил отдельные папки для Групп и Контактов. Почистил тест в контактов от лишних заполнений полей

---
+++ src/test/java/ru/stqa/pft/addressbook/Contact/ContactCreationTests.java
@@ -0,0 +1,74 @@
+package ru.stqa.pft.addressbook.Contact;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactCreationTests {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+      login("admin", "secret");
+
+  }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+  public void testContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("asd1");
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys("asd1");
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys("cbndfgsdfg wwert1");
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys("1231231233");
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("asd1@sa.asd");
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    wd.findElement(By.linkText("home page")).click();
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
