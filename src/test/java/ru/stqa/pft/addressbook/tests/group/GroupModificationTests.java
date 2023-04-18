86e860546cbd7f95674fc80729fb495534a37671
Alex_Donskov <mrad@list.ru>
2023-04-18 22:06:11 +0300

    Исправил ошибку в названии класса GroupModificationTests

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
-public class GroupModificationTest extends TestBase{
+public class GroupModificationTests extends TestBase{
 
     @Test
     public void testGroupModification () {
