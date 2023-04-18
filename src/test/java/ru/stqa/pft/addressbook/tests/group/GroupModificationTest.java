e428c20d9446c5c2de4c1b75cdd489fc7642d963
Alex_Donskov <mrad@list.ru>
2023-04-18 22:03:58 +0300

    Создал testGroupModification с дополнительными методами в GroupHelper

---
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests.group;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.tests.TestBase;
+
+public class GroupModificationTest extends TestBase{
+
+    @Test
+    public void testGroupModification () {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("New_Name", "New_Header", "New_Footer"));
+        app.getGroupHelper().submitGroupModification();
+    }
+}
