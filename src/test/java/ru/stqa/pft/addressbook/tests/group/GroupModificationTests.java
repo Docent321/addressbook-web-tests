2fff88edbd25e686ebac60f4f17f9a03d9daf9e3
Alex_Donskov <mrad@list.ru>
2023-05-01 16:18:26 +0300

    Изменил тесты для удаления и модификации групп. Теперь выбирается последняя запись в таблице

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase{
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("Name", "Header", "Footer"));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("New_Name", "New_Header", "New_Footer"));
         app.getGroupHelper().submitGroupModification();
