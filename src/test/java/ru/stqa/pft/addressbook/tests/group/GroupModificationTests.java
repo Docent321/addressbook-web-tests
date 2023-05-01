5ea6815d160c2c6ab0971804104a2fb71986e2db
Alex_Donskov <mrad@list.ru>
2023-05-01 17:09:34 +0300

    Модифицировал тесты для групп. Теперь в проверках сравниваем размер списков.

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -5,21 +5,23 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase{
 
     @Test
     public void testGroupModification () {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("Name", "Header", "Footer"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("New_Name", "New_Header", "New_Footer"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
