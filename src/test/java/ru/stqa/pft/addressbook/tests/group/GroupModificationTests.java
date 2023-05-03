77e1443d15c3978d59e6011e0e695432331e7751
Alex_Donskov <mrad@list.ru>
2023-05-03 09:39:37 +0300

    Изменил тест для модификации Группы

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -18,10 +19,15 @@ public class GroupModificationTests extends TestBase{
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("New_Name", "New_Header", "New_Footer"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"New_Name", "New_Header", "New_Footer");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 }
