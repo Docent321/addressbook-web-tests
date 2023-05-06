c2a033d83e3a9b156382e790ab1f1b6e48dd5b07
Alex_Donskov <mrad@list.ru>
2023-05-06 23:15:16 +0300

    Изменил тест создания контакта. Добавил использование библиотеки Hamcrest

--- src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactCreationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests.contact;
 
-import org.testng.Assert;
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.tests.TestBase;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
 
@@ -13,17 +15,15 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.goTo().contactPage();
-    Set<ContactData> before = app.contact().all();
+    Contacts before = app.contact().all();
     ContactData contact = new ContactData()
             .withFirstname("asd4").withLastname("asd4")
             .withAddress("cbndfgsdfg").withPhone("1231231233")
             .withEmail("asd1@sa.asd");
     app.contact().create(contact);
-    Set<ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(), before.size() + 1);
-
-    contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.contact().all();
+    assertEquals(after.size(), before.size() + 1);
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
 }
