632e563019c55cdbc7ba5cacbdac5ce5847e95e0
Alex_Donskov <mrad@list.ru>
2023-05-06 20:44:01 +0300

    Изменил тест для создания группы, теперь работает через Set

--- src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends BaseHelper {
 
@@ -85,7 +87,17 @@ public class GroupHelper extends BaseHelper {
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            //GroupData group = new GroupData().withId(id).withName(name);
+            groups.add(new GroupData().withId(id).withName(name));
+        }
+        return groups;
+    }
+
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new  HashSet <GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             groups.add(new GroupData().withId(id).withName(name));
         }
         return groups;
