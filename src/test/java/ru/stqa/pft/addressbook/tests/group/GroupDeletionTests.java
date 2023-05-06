eafc5e5056ec28c354039560ecaf8efe5e050ddf
Alex_Donskov <mrad@list.ru>
2023-05-06 21:12:21 +0300

    Изменил тест для модификации группы. Почистил код от неиспользуемых методов и импортов

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTests.java
@@ -5,7 +5,6 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
-import java.util.List;
 import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
@@ -13,7 +12,7 @@ public class GroupDeletionTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0){
+    if (app.group().all().size() == 0){
       app.group().create(new GroupData().withName("Name"));
     }
   }
