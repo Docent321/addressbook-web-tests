81294449662c83e9e55f2a8bef62bebf577da18f
Alex_Donskov <mrad@list.ru>
2023-05-06 22:55:38 +0300

    Изменил тест модификации контакта. Добавил использование библиотеки Hamcrest

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -1,12 +1,15 @@
 package ru.stqa.pft.addressbook.tests.group;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
-import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase{
 
@@ -20,17 +23,14 @@ public class GroupModificationTests extends TestBase{
 
     @Test
     public void testGroupModification () {
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData modifyGroup  = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifyGroup.getId()).withName("N_Name").withHeader("N_Header").withFooter("N_Footer");
         app.group().modify(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
-
-        before.remove(modifyGroup);
-        before.add(group);
-        Assert.assertEquals(before, after);
+        Groups after = app.group().all();
+        assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before.without(modifyGroup).withAdded(group)));
     }
 
 }
