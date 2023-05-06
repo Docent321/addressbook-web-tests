1923459f9bb9dcafc60368830f3b4f002479cfa0
Alex_Donskov <mrad@list.ru>
2023-05-06 19:41:16 +0300

    Ипользовал Fluent-интерфейсы для тестов Создания, Удлаения и Модификации групп

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTests.java
@@ -14,7 +14,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("New_Name1", "New_Header", "New_Footer");
+    GroupData group = new GroupData().withName("N_Name1").withHeader("N_Header").withFooter("N_Footer1");
     app.group().create(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTests.java
@@ -13,7 +13,7 @@ public class GroupDeletionTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0){
-      app.group().create(new GroupData("Name", "Header", "Footer"));
+      app.group().create(new GroupData().withName("Name"));
     }
   }
 
