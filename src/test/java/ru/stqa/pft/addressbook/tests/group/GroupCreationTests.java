45f71e0b3f33d10c2087e50cdad33cd335f576ae
Alex_Donskov <mrad@list.ru>
2023-05-13 16:14:53 +0300

    Добавил DataProvider для создания групп

--- src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTests.java
@@ -5,16 +5,28 @@ import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.tests.TestBase;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("test1").withFooter("test1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("test2").withFooter("test2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("test3").withFooter("test3")});
+    return list.listIterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("N_Name1").withHeader("N_Header").withFooter("N_Footer1");
     app.group().create(group);
     Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
