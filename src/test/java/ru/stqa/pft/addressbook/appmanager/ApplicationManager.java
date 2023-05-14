050f74ab48cdc26cfd60a6c4039752b190ee0efb
Alex_Donskov <mrad@list.ru>
2023-05-14 17:19:28 +0300

    Добавил выбор базовго урла, логина и пароля из файла local.properties

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -16,12 +22,16 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
+
         this.browser = String.valueOf(browser);
+        properties = new Properties();
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         if (browser.equals("CHROME")){
             wd = new ChromeDriver();
         }
@@ -30,12 +40,12 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook");
+        wd.get(properties.getProperty("web.baseUrl"));
         sessionHelper = new SessionHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
 
--- src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,17 +2,21 @@ package ru.stqa.pft.addressbook.tests;
 
 
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager("FIREFOX");
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", "CHROME"));
+
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
 
     }
